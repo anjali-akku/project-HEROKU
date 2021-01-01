262e1a76039f0eca7ef754c789f8abf95720cfad
anjali-akku <anjali.akku.nk@gmail.com>
2021-01-01 13:20:21 +0530

    heroku-third

--- django_medcare/settings.py
+++ django_medcare/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,4 +128,5 @@ STATICFILES_DIRS=[
 
 ]
 
-STATICFILES_STORAGE= 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE= 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
